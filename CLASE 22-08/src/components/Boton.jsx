327805e8010f885b83e1518a20242c22f9a0316a
Laura Camila Vega <160734809+lauris-camila@users.noreply.github.com>
2024-08-23 06:47:27 -0500

    Actividad React usando Vite

---
+++ CLASE 22-08/src/components/Boton.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+// const Boton = () => {
+//     const [state, setstate] =useState("Hola");
+//     setstate("Adios");
+  
+//     return (
+//         <div>
+//             <p>{state}</p>
+
+//             <button onClick={handleButton}>Cambiar saludo a despido</button>
+//         <div/>
+
+//     );
+// };
+
+
